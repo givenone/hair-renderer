f479fba36e56571091f84b98fba7f4f32f30df60
givenone <givenone1@gmail.com>
2020-07-13 17:20:05 +0900

    revised compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ src/Hair.cc src/shader.cpp src/controls.cpp viewer.cpp -o hairviewer -Iinclude -Ivendor/trimesh2/include -lglut -lGL -lGLU -lGLEW `pkg-config --static --libs glfw3`
+g++ src/Hair.cc src/shader.cpp src/controls.cpp viewer.cpp -o hairviewer -Iinclude -Ivendor/trimesh2/include -lglfw -lglut -lGL -lGLU -lGLEW `pkg-config --cflags --libs opencv`
